02149d2ddad2022e6a57c3f577492ff19abef490
Roland Vincent <roland.vincentnet@gmail.com>
2022-05-30 13:54:32 +0700

    Update mysql port

--- .flaskenv
+++ .flaskenv
@@ -5,3 +5,4 @@ MYSQL_HOST=127.0.0.1
 MYSQL_USER=developer
 MYSQL_PASSWORD=googlesql@bangkit2022
 MYSQL_DB=true-sight
+MYSQL_PORT=3306
--- app.py
+++ app.py
@@ -24,6 +24,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
+app.config['MYSQL_PORT'] = os.getenv('MYSQL_PORT')
 app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
