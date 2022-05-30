bdba78c2e0e32fa4460302142ce939fb00abcc84
FachryIkhsal890 <fachryikhsal890@gmail.com>
2022-05-30 16:13:51 +0700

    remove flask_mysqlDB

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 from flask import Flask, request
 from flask import jsonify
 from flask_bcrypt import Bcrypt
-from flask_mysqldb import MySQL
 from database import Database
 from datetime import datetime
 from helper import *
@@ -24,12 +23,6 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-app.config['MYSQL_UNIX_SOCKET'] = os.getenv('MYSQL_UNIX_SOCKET')
-mysql = MySQL(app)
 # Bcrypt encrypt and check support
 bcrypt = Bcrypt(app)
 logger = Logger()
